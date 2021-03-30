211e4b8f3487920d10749ca8f05c882878009e53
Yash Paddalwar <66002863+YashP-0701@users.noreply.github.com>
2021-03-30 20:24:13 +0530

    Update bill.py

--- bill.py
+++ bill.py
@@ -4,13 +4,14 @@ from tkinter import ttk;
 import mysql.connector
 import random
 from tkinter import messagebox
+from pyscreenshot import grab
 
 
 class GenerateBill:
     def __init__(self,root):
         self.root=root
         self.root.title("Hotel Management System")
-        self.root.geometry("400x550+600+220")
+        self.root.geometry("400x510+0+0")
 
 
         #    """""""""""""""""""""""Variables""""""""""""""""""""""""
@@ -20,8 +21,8 @@ class GenerateBill:
 
         #    """""""""""""""""""""""Title""""""""""""""""""""""""
 
-        lbl_title = Label(self.root,text="Bill",font=("times new roman",10,"bold"),bg="black",fg="white",bd=4,relief=RIDGE)
-        lbl_title.place(x=0,y=0,width=400,height=40)
+        lbl_title = Label(self.root,text="WELCOME TO HOTEL DELUXE",font=("times new roman",10,"bold"),bg="black",fg="white",bd=4,relief=RIDGE)
+        lbl_title.place(x=0,y=65,width=400,height=40)
 
         #    """""""""""""""""""""""Logo""""""""""""""""""""""""
 
@@ -30,46 +31,46 @@ class GenerateBill:
         self.photoimglogo = ImageTk.PhotoImage(imglogo)
 
         lblimg = Label(self.root,image=self.photoimglogo,bd=0,relief=RIDGE)
-        lblimg.place(x=0,y=0,width=50,height=40)
+        lblimg.place(x=0,y=65,width=50,height=40)
 
 
         #    """""""""""""""""""""""Button Frame""""""""""""""""""""""""
 
-        btn_frame = Frame(self.root,bd=2,relief=RIDGE)
-        btn_frame.place(x=0,y=40,width=400,height=60)
+        btn_frame = Frame(self.root,bd=2,bg="#9620bd",relief=RIDGE)
+        btn_frame.place(x=0,y=0,width=400,height=60)
 
         # Cust_Contact
-        lbl_contact = Label(btn_frame,text="Customer Contact",fg="black",font=("times new roman",10,"bold"),padx=2,pady=6)
-        lbl_contact.grid(row=0,column=0,sticky=W)
+        lbl_contact = Label(btn_frame,text="Customer Contact",bg="#9620bd",fg="white",font=("times new roman",10,"bold"),padx=2,pady=6)
+        lbl_contact.grid(row=0,column=0,sticky=W,padx=6)
 
         entry_contact = ttk.Entry(btn_frame,textvariable=self.var_contact,width=20,font=("times new roman",10,"bold"))
-        entry_contact.grid(row=0,column=1,sticky=W)
+        entry_contact.grid(row=0,column=1,sticky=W,padx=6)
 
 
         #    """""""""""""""""""""""Bill Button""""""""""""""""""""""""
-        buttonBill = Button(btn_frame,text="Generate Bill",command=self.Fetch_Details_customer,font=("times new roman",10,"bold"),bg="black",fg="white",width=10,cursor="hand2")
-        buttonBill.grid(row=0,column=2,sticky=W)
+        buttonBill = Button(btn_frame,text="Generate Bill",command=self.Fetch_Details_customer,font=("times new roman",10,"bold"),bg="#bd205c",fg="white",width=10,cursor="hand2")
+        buttonBill.grid(row=0,column=2,sticky=W,padx=6)
 
         # Cust_Room
-        lbl_Room = Label(btn_frame,text="Room No",fg="black",font=("times new roman",10,"bold"),padx=2,pady=6)
-        lbl_Room.grid(row=1,column=0,sticky=W)
+        lbl_Room = Label(btn_frame,text="Room No",bg="#9620bd",fg="white",font=("times new roman",10,"bold"),padx=2,pady=6)
+        lbl_Room.grid(row=1,column=0,sticky=W,padx=6)
 
         entry_Room = ttk.Entry(btn_frame,textvariable=self.var_roomavailable,width=20,font=("times new roman",10,"bold"))
-        entry_Room.grid(row=1,column=1,sticky=W)
+        entry_Room.grid(row=1,column=1,sticky=W,padx=6)
 
 
         #    """""""""""""""""""""""Bill Button""""""""""""""""""""""""
-        buttonBillroom = Button(btn_frame,text="Generate Bill",command=self.Fetch_Details_room,font=("times new roman",10,"bold"),bg="black",fg="white",width=10,cursor="hand2")
-        buttonBillroom.grid(row=1,column=2,sticky=W)
+        buttonBillroom = Button(btn_frame,text="Generate Bill",command=self.Fetch_Details_room,font=("times new roman",10,"bold"),bg="#bd205c",fg="white",width=10,cursor="hand2")
+        buttonBillroom.grid(row=1,column=2,sticky=W,padx=6)
 
-        #    """""""""""""""""""""""Bill Frame""""""""""""""""""""""""
+        #    """""""""""""""""""""""Print Bill Frame""""""""""""""""""""""""
 
-        printbill_frame = Frame(self.root,bd=0,relief=RIDGE)
-        printbill_frame.place(x=0,y=490,width=400,height=30)
+        printbill_frame = Frame(self.root,bg="#9620bd",bd=0,relief=RIDGE)
+        printbill_frame.place(x=0,y=480,width=400,height=30)
 
         #    """""""""""""""""""""""Bill Button""""""""""""""""""""""""
-        buttonBillroom = Button(printbill_frame,text="Print Bill",font=("times new roman",10,"bold"),bg="black",fg="white",width=10,cursor="hand2")
-        buttonBillroom.grid(row=1,column=2,sticky=W)
+        buttonBillroom = Button(printbill_frame,text="Print Bill",command=self.Imagess,font=("times new roman",10,"bold"),bg="#bd205c",fg="white",width=10,cursor="hand2",relief=GROOVE)
+        buttonBillroom.grid(row=1,column=2,sticky=W,padx=4)
 
         
     def Fetch_Details_customer(self):
@@ -92,14 +93,14 @@ class GenerateBill:
 
                 #    """""""""""""""""""""""Bill Frame""""""""""""""""""""""""
 
-                bill_frame = Frame(self.root,bd=0,relief=RIDGE)
+                bill_frame = Frame(self.root,bd=0,bg="#1812c9",relief=RIDGE)
                 bill_frame.place(x=0,y=102,width=400,height=150)
                 
 
-                lblName = Label(bill_frame,text="Name:",font=("times new roman",12,"bold"))
+                lblName = Label(bill_frame,text="Name:",bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lblName.place(x=0,y=0)
 
-                lbl = Label(bill_frame,text=row,font=("times new roman",12,"bold"))
+                lbl = Label(bill_frame,text=row,bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbl.place(x=90,y=0)
 
                 #    """""""""""""""""""""""Gender""""""""""""""""""""""""
@@ -111,10 +112,10 @@ class GenerateBill:
                 my_cursor.execute(query,value)
                 row = my_cursor.fetchone()
 
-                lblGender = Label(bill_frame,text="Gender:",font=("times new roman",12,"bold"))
+                lblGender = Label(bill_frame,text="Gender:",bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lblGender.place(x=0,y=30)
 
-                lbl2 = Label(bill_frame,text=row,font=("times new roman",12,"bold"))
+                lbl2 = Label(bill_frame,text=row,bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbl2.place(x=90,y=30)
 
                 #    """""""""""""""""""""""Email""""""""""""""""""""""""
@@ -126,10 +127,10 @@ class GenerateBill:
                 my_cursor.execute(query,value)
                 row = my_cursor.fetchone()
 
-                lblEmail = Label(bill_frame,text="Email:",font=("times new roman",12,"bold"))
+                lblEmail = Label(bill_frame,text="Email:",bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lblEmail.place(x=0,y=60)
 
-                lbl3 = Label(bill_frame,text=row,font=("times new roman",12,"bold"))
+                lbl3 = Label(bill_frame,text=row,bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbl3.place(x=90,y=60)
 
                 #    """""""""""""""""""""""Nationality""""""""""""""""""""""""
@@ -141,10 +142,10 @@ class GenerateBill:
                 my_cursor.execute(query,value)
                 row = my_cursor.fetchone()
 
-                lblNationality = Label(bill_frame,text="Nationality:",font=("times new roman",12,"bold"))
+                lblNationality = Label(bill_frame,text="Nationality:",bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lblNationality.place(x=0,y=90)
 
-                lbl4 = Label(bill_frame,text=row,font=("times new roman",12,"bold"))
+                lbl4 = Label(bill_frame,text=row,bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbl4.place(x=90,y=90)
 
                 #    """""""""""""""""""""""Address""""""""""""""""""""""""
@@ -156,10 +157,10 @@ class GenerateBill:
                 my_cursor.execute(query,value)
                 row = my_cursor.fetchone()
 
-                lblAddress = Label(bill_frame,text="Address:",font=("times new roman",12,"bold"))
+                lblAddress = Label(bill_frame,text="Address:",bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lblAddress.place(x=0,y=120)
 
-                lbl5 = Label(bill_frame,text=row,font=("times new roman",12,"bold"))
+                lbl5 = Label(bill_frame,text=row,bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbl5.place(x=90,y=120)
 
                 
@@ -185,14 +186,14 @@ class GenerateBill:
 
                 #    """""""""""""""""""""""Check In""""""""""""""""""""""""
 
-                bill_frame1 = Frame(self.root,bd=0,relief=RIDGE)
+                bill_frame1 = Frame(self.root,bg="#1812c9",bd=0,relief=RIDGE)
                 bill_frame1.place(x=0,y=252,width=400,height=220)
                 
 
-                lblcheckin = Label(bill_frame1,text="Check-in:",font=("times new roman",12,"bold"))
+                lblcheckin = Label(bill_frame1,text="Check-in:",bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lblcheckin.place(x=0,y=0)
 
-                lbl20 = Label(bill_frame1,text=row,font=("times new roman",12,"bold"))
+                lbl20 = Label(bill_frame1,text=row,bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbl20.place(x=90,y=0)
 
 
@@ -206,10 +207,10 @@ class GenerateBill:
                 my_cursor.execute(query,value)
                 row = my_cursor.fetchone()
 
-                lblcheckout = Label(bill_frame1,text="Check-Out:",font=("times new roman",12,"bold"))
+                lblcheckout = Label(bill_frame1,text="Check-Out:",bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lblcheckout.place(x=0,y=30)
 
-                lbl21 = Label(bill_frame1,text=row,font=("times new roman",12,"bold"))
+                lbl21 = Label(bill_frame1,text=row,bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbl21.place(x=90,y=30)
 
 
@@ -223,10 +224,10 @@ class GenerateBill:
                 my_cursor.execute(query,value)
                 row = my_cursor.fetchone()
 
-                lblType = Label(bill_frame1,text="Room-Type:",font=("times new roman",12,"bold"))
+                lblType = Label(bill_frame1,text="Room-Type:",bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lblType.place(x=0,y=60)
 
-                lbl22 = Label(bill_frame1,text=row,font=("times new roman",12,"bold"))
+                lbl22 = Label(bill_frame1,text=row,bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbl22.place(x=90,y=60)
 
                 #    """""""""""""""""""""""Meal""""""""""""""""""""""""
@@ -239,10 +240,10 @@ class GenerateBill:
                 my_cursor.execute(query,value)
                 row = my_cursor.fetchone()
 
-                lblMeal = Label(bill_frame1,text="Meal:",font=("times new roman",12,"bold"))
+                lblMeal = Label(bill_frame1,text="Meal:",bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lblMeal.place(x=0,y=90)
 
-                lbl23 = Label(bill_frame1,text=row,font=("times new roman",12,"bold"))
+                lbl23 = Label(bill_frame1,text=row,bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbl23.place(x=90,y=90)
 
 
@@ -256,10 +257,10 @@ class GenerateBill:
                 my_cursor.execute(query,value)
                 row = my_cursor.fetchone()
 
-                lblpaidtax = Label(bill_frame1,text="Paid Tax:",font=("times new roman",12,"bold"))
+                lblpaidtax = Label(bill_frame1,text="Paid Tax:",bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lblpaidtax.place(x=0,y=120)
 
-                lbl6 = Label(bill_frame1,text=row,font=("times new roman",12,"bold"))
+                lbl6 = Label(bill_frame1,text=row,bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbl6.place(x=90,y=120)
 
             
@@ -273,10 +274,10 @@ class GenerateBill:
                 my_cursor.execute(query,value)
                 row = my_cursor.fetchone()
 
-                lblsubtotal = Label(bill_frame1,text="Sub-Total:",font=("times new roman",12,"bold"))
+                lblsubtotal = Label(bill_frame1,text="Sub-Total:",bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lblsubtotal.place(x=0,y=150)
 
-                lbl7 = Label(bill_frame1,text=row,font=("times new roman",12,"bold"))
+                lbl7 = Label(bill_frame1,text=row,bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbl7.place(x=90,y=150)
 
                 #    """""""""""""""""""""""Total""""""""""""""""""""""""
@@ -288,17 +289,15 @@ class GenerateBill:
                 my_cursor.execute(query,value)
                 row = my_cursor.fetchone()
 
-                lbltotal = Label(bill_frame1,text="Total:",font=("times new roman",12,"bold"))
+                lbltotal = Label(bill_frame1,text="Total:",bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbltotal.place(x=0,y=180)
 
-                lbl8 = Label(bill_frame1,text=row,font=("times new roman",12,"bold"))
+                lbl8 = Label(bill_frame1,text=row,bg="#1812c9",fg="white",font=("times new roman",12,"bold"))
                 lbl8.place(x=90,y=180)
 
-
-
-
-
-    
+    def Imagess(self):
+        im = grab(bbox=(10,125,450,620))
+        im.show()
 
 
 
